00fa85c7ba8cb39c52982470f20f36d4347a9faf
Pablo Parreño Domínguez <pablo.parreno.ext@rsi.cajarural.com>
2025-10-21 13:13:48 +0200

    otro intento

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os
 from supabase import create_client, Client
 app = Flask(__name__)
 # 🔗 Configuración desde variables de entorno
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://bxgpbkimoqyzfnehgnhh.supabase.co")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Z3Bia2ltb3F5emZuZWhnbmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMzc1NjMsImV4cCI6MjA3NjYxMzU2M30.dTaMH4d0hNUd3OP740stlhb5lvmG8ufgSWqNFHwtFTs")
 # Inicializa el cliente de Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 def get_data():
