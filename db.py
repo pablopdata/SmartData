5bf56e2dca88042011b435bb427de7be00aee212
pablopdata <pparreno@stemdo.io>
2025-10-28 16:37:14 +0100

    fix

--- db.py
+++ db.py
@@ -2,9 +2,9 @@ from supabase import create_client, Client
 
 # ✅ Configura tu conexión a Supabase
 
-SUPABASE_URL = "TU_SUPABASE_URL"
+SUPABASE_URL = "SUPABASE_URL"
 
-SUPABASE_KEY = "TU_SUPABASE_KEY"
+SUPABASE_KEY = "SUPABASE_KEY"
 
 # 🔹 Crea el cliente global de conexión
 
