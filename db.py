026ef11a4f674bb749d3c5afb540eeb70d96ef41
jalejo <jalejo@stemdo.io>
2025-11-03 15:36:02 +0100

    añadidas credenciales supabase

--- db.py
+++ db.py
@@ -5,9 +5,9 @@ import os
 
 # Configura tu conexión a Supabase
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_URL = os.getenv("https://bxgpbkimoqyzfnehgnhh.supabase.co")
 
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Z3Bia2ltb3F5emZuZWhnbmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMzc1NjMsImV4cCI6MjA3NjYxMzU2M30.dTaMH4d0hNUd3OP740stlhb5lvmG8ufgSWqNFHwtFTs")
 
 # 🔹 Crea el cliente global de conexión
 
